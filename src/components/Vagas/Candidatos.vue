edf2be50cc4b3eb0a6f94799db0574d1f6b18c94
Fernando <nando.online@live.com>
2020-10-19 07:14:16 -0300

    Separe layout

--- src/components/Vagas/Candidatos.vue
+++ src/components/Vagas/Candidatos.vue
@@ -3,11 +3,41 @@
 		<b-col>
 			<b-row>
 				<b-col>
-					<h3>Volta!</h3>
+					<b-list-group-item class="d-flex align-items-center candidato" v-for="candidato in candidatos" :key="candidato.id">
+						<b-avatar variant="info" :src="'data:image/jpeg;base64,' + candidato.image" class="mr-3"></b-avatar>
+						<span class="mr-auto" :title="candidato.nome">{{candidato.nome.substring(0,10)}}</span>
+						<b-icon class="icon" icon="bookmark-fill" title="Favorito" v-if="candidato.favorito == 1"></b-icon>
+						<b-icon class="icon" icon="bookmark" v-else></b-icon>
+						<b-icon class="icon icon-view" icon="aspect-ratio" title="Ver candidato"  @click="buscarCanditato(candidato.id)"></b-icon>
+					</b-list-group-item>
+				</b-col>
+				<b-col cols="9">
+					<b-card no-body v-if="candidato">
+						<b-tabs pills card vertical>
+							<b-tab title="Início" active><b-card-text>
+								<h3>{{candidato.nome}}</h3>
+								<p class="comentario">{{candidato.descricao || 'Não tem nenhum comentário!' }}</p>
+								<span class="objetivo"><strong>Objetivo:</strong> {{candidato.objetivo}}</span>
+							</b-card-text></b-tab>
+							<b-tab title="Cursos"><b-card-text>
+								<b-list-group>
+									<b-list-group-item href="#" disabled class="flex-column align-items-start" v-for="curso in candidato.cursos" :key="curso.id">
+										<div class="d-flex w-100 justify-content-between">
+											<h5 class="mb-1">{{curso.instituicao}} - {{curso.nome}}</h5>
+											<small class="text-muted"><strong>Inicio:</strong>{{curso.inicio}} <strong>Fim:</strong>{{curso.fim}} - {{curso.status}}</small>
+										</div>
+									</b-list-group-item>
+								</b-list-group>
+							</b-card-text></b-tab>
+							<b-tab title="Escolaridade"><b-card-text>Escolaridade</b-card-text></b-tab>
+							<b-tab title="Informações"><b-card-text>Informações</b-card-text></b-tab>
+						</b-tabs>
+					</b-card>
 				</b-col>
 			</b-row>
 		</b-col>
 	</div>
+
 </template>
 
 <script>
--- src/components/Vagas/Index.vue
+++ src/components/Vagas/Index.vue
@@ -2,36 +2,14 @@
 	<div class="conteudo">
 		<b-col>
 			<b-row>
-				<b-col>
-					<b-list-group-item class="d-flex align-items-center candidato" v-for="candidato in candidatos" :key="candidato.id">
-						<b-avatar variant="info" :src="'data:image/jpeg;base64,' + candidato.image" class="mr-3"></b-avatar>
-						<span class="mr-auto" :title="candidato.nome">{{candidato.nome.substring(0,10)}}</span>
-						<b-icon class="icon" icon="bookmark-fill" title="Favorito" v-if="candidato.favorito == 1"></b-icon>
-						<b-icon class="icon" icon="bookmark" v-else></b-icon>
-						<b-icon class="icon icon-view" icon="aspect-ratio" title="Ver candidato"  @click="buscarCanditato(candidato.id)"></b-icon>
-					</b-list-group-item>
-				</b-col>
-				<b-col cols="9">
-					<b-card no-body v-if="candidato">
-						<b-tabs pills card vertical>
-							<b-tab title="Início" active><b-card-text>
-								<h3>{{candidato.nome}}</h3>
-								<p class="comentario">{{candidato.descricao || 'Não tem nenhum comentário!' }}</p>
-								<span class="objetivo"><strong>Objetivo:</strong> {{candidato.objetivo}}</span>
-							</b-card-text></b-tab>
-							<b-tab title="Cursos"><b-card-text>
-								<b-list-group>
-									<b-list-group-item href="#" disabled class="flex-column align-items-start" v-for="curso in candidato.cursos" :key="curso.id">
-										<div class="d-flex w-100 justify-content-between">
-											<h5 class="mb-1">{{curso.instituicao}} - {{curso.nome}}</h5>
-											<small class="text-muted"><strong>Inicio:</strong>{{curso.inicio}} <strong>Fim:</strong>{{curso.fim}} - {{curso.status}}</small>
-										</div>
-									</b-list-group-item>
-								</b-list-group>
-							</b-card-text></b-tab>
-							<b-tab title="Escolaridade"><b-card-text>Escolaridade</b-card-text></b-tab>
-							<b-tab title="Informações"><b-card-text>Informações</b-card-text></b-tab>
-						</b-tabs>
+				<b-col v-for="vaga in vagas" :key="vaga.id">
+					<b-card :header="vaga.titulo">
+						<blockquote class="blockquote mb-0">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+							<footer class="blockquote-footer">
+								Someone famous in <cite title="Source Title">Source Title</cite>
+							</footer>
+						</blockquote>
 					</b-card>
 				</b-col>
 			</b-row>
@@ -50,18 +28,17 @@ export default {
 	},
 	data () {
 		return {
-			candidatos: [],
-			candidato: 0,
+			vagas: []
 		}
 	},
 	computed: {
 	},
 	mounted() {
-		this.getCadidatos();
+		this.getVagas();
 	},
 	methods: {
-		getCadidatos() {
-			axios.get("http://localhost:3000/candidatos", {
+		getVagas() {
+			axios.get("http://localhost:3000/vagas", {
 				headers: {
 					"Content-Type": "application/json",
 					// Authorization: "Bearer " + this.user.token
@@ -69,30 +46,12 @@ export default {
 			})
 			.then(response => {
 				console.log(response.data);
-				this.candidatos = response.data;
+				this.vagas = response.data;
 			})
 			.catch(e => {
 				console.log(e);
 				alert("servidor fora de área");
 			});
-		},
-
-		buscarCanditato(id_cadidato){
-			axios.get("http://localhost:3000/candidatos/" + id_cadidato, {
-				// axios.get('http://localhost:8000/api/produto', {
-				headers: {
-					"Content-Type": "application/json",
-					// Authorization: "Bearer " + this.user.token
-				}
-			})
-				.then(response => {
-					console.log(response.data);
-					this.candidato = response.data;
-				})
-				.catch(e => {
-					console.log(e);
-					alert("servidor fora de área");
-				});
 		},
 	}
 };
